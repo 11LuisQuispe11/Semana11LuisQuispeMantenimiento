537d2f4a975e7d0e1dd3ec8c8fb90cd013d0bc84
Luis <79956765+11LuisQuispe11@users.noreply.github.com>
2023-06-08 09:54:01 -0500

    Update Queen.java V1

--- Original.src/Queen.java
+++ Original.src/Queen.java
@@ -1,81 +1,56 @@
 import javax.swing.ImageIcon;
 import java.util.ArrayList;
-// import java.awt.Color;
-// -------------------------------------------------------------------------
-/**
- * Represents a Queen game piece.
- *
- * @author Ben Katz (bakatz)
- * @author Myles David II (davidmm2)
- * @author Danielle Bushrow (dbushrow)
- * @version 2010.11.17
- */
-public class Queen
-    extends ChessGamePiece{
-    // ----------------------------------------------------------
-    /**
-     * Create a new Queen object.
-     *
-     * @param board
-     *            the board the queen is on
-     * @param row
-     *            the row location of the queen
-     * @param col
-     *            the column location of the queen
-     * @param color
-     *            either GamePiece.WHITE, BLACK, or UNASSIGNED
-     */
-    public Queen( ChessGameBoard board, int row, int col, int color ){
-        super( board, row, col, color );
+
+public class Queen extends ChessGamePiece {
+    private ChessGameBoard board;
+    private int row;
+    private int col;
+    private int color;
+
+    public Queen(ChessGameBoard board, int row, int col, int color) {
+        super(board, row, col, color);
+        this.board = board;
+        this.row = row;
+        this.col = col;
+        this.color = color;
     }
-    /**
-     * Calculates the possible moves for this Queen.
-     * @param board the board to check on
-     * @return ArrayList<String> the list of moves
-     */
+
     @Override
-    protected ArrayList<String> calculatePossibleMoves( ChessGameBoard board ){
-        ArrayList<String> northEastMoves = calculateNorthEastMoves( board, 8 );
-        ArrayList<String> northWestMoves = calculateNorthWestMoves( board, 8 );
-        ArrayList<String> southEastMoves = calculateSouthEastMoves( board, 8 );
-        ArrayList<String> southWestMoves = calculateSouthWestMoves( board, 8 );
-        ArrayList<String> northMoves = calculateNorthMoves( board, 8 );
-        ArrayList<String> southMoves = calculateSouthMoves( board, 8 );
-        ArrayList<String> eastMoves = calculateEastMoves( board, 8 );
-        ArrayList<String> westMoves = calculateWestMoves( board, 8 );
-        ArrayList<String> allMoves = new ArrayList<String>();
-        allMoves.addAll( northEastMoves );
-        allMoves.addAll( northWestMoves );
-        allMoves.addAll( southWestMoves );
-        allMoves.addAll( southEastMoves );
-        allMoves.addAll( northMoves );
-        allMoves.addAll( southMoves );
-        allMoves.addAll( westMoves );
-        allMoves.addAll( eastMoves );
+    protected ArrayList<String> calculatePossibleMoves(ChessGameBoard board) {
+        ArrayList<String> allMoves = calculateMovesInAllDirections(board, 8);
         return allMoves;
     }
-    /**
-     * Creates an icon for this piece depending on the piece's color.
-     *
-     * @return ImageIcon the ImageIcon representation of this piece.
-     */
+
+    // Técnica utilizada: Extract Method
+    private ArrayList<String> calculateMovesInAllDirections(ChessGameBoard board, int distance) {
+        ArrayList<String> moves = new ArrayList<String>();
+        moves.addAll(calculateNorthEastMoves(board, distance));
+        moves.addAll(calculateNorthWestMoves(board, distance));
+        moves.addAll(calculateSouthEastMoves(board, distance));
+        moves.addAll(calculateSouthWestMoves(board, distance));
+        moves.addAll(calculateNorthMoves(board, distance));
+        moves.addAll(calculateSouthMoves(board, distance));
+        moves.addAll(calculateWestMoves(board, distance));
+        moves.addAll(calculateEastMoves(board, distance));
+        return moves;
+    }
+
+    // Resto del código sin cambios
+
     @Override
-    public ImageIcon createImageByPieceType(){
-        if ( getColorOfPiece() == ChessGamePiece.WHITE ){
-            return new ImageIcon(
-                getClass().getResource("chessImages/WhiteQueen.gif")
-            );            
-        }
-        else if ( getColorOfPiece() == ChessGamePiece.BLACK ){
-            return new ImageIcon(
-                getClass().getResource("chessImages/BlackQueen.gif")
-            );            
-        }
-        else
-        {
-            return new ImageIcon(
-                getClass().getResource("chessImages/default-Unassigned.gif")
-            ); 
+    public ImageIcon createImageByPieceType() {
+        String imagePath = getImagePath();
+        return new ImageIcon(getClass().getResource(imagePath));
+    }
+
+    // Técnica utilizada: Extract Method
+    private String getImagePath() {
+        if (getColorOfPiece() == ChessGamePiece.WHITE) {
+            return "chessImages/WhiteQueen.gif";
+        } else if (getColorOfPiece() == ChessGamePiece.BLACK) {
+            return "chessImages/BlackQueen.gif";
+        } else {
+            return "chessImages/default-Unassigned.gif";
         }
     }
 }
